ac532e7a2329fe6440711946f50cdd0c32aa17ad
hbakl <hbakl@hotmail.com>
2024-10-20 04:08:51 +0300

    The project has started, the database and the initial design that users will see have been completed. Additionally, the Admin theme has been added.

---
+++ MvcCv/Controllers/DefaultController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCv.Models.Entity;
+
+namespace MvcCv.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        DbCvEntities db =new DbCvEntities();
+        public ActionResult Index()
+        {
+            var degerler = db.TblAbout.ToList();
+            return View(degerler);
+        }
+
+        public PartialViewResult Deneyim()
+        {
+            var deneyimler = db.TblExperience.ToList();
+            return PartialView(deneyimler);
+        }
+
+        public PartialViewResult Egitimlerim()
+        {
+            var egitimler = db.TblEducation.ToList();
+            return PartialView(egitimler);
+        }
+
+        public PartialViewResult Yeteneklerim()
+        {
+            var yetenekler = db.TblSkill.ToList();
+            return PartialView(yetenekler);
+        }
+
+        public PartialViewResult Hobilerim()
+        {
+            var hobiler = db.TblInterest.ToList();
+            return PartialView(hobiler);
+        }
+        public PartialViewResult Sertifikalar()
+        {
+            var sertifikalar = db.TblCertificate.ToList();
+            return PartialView(sertifikalar);
+        }
+
+
+        [HttpGet]
+        public PartialViewResult iletisim()
+        {
+                return PartialView();
+        }
+
+
+        [HttpPost]
+        public PartialViewResult iletisim(TblContact t)
+        {
+            t.Date = DateTime.Now;
+            db.TblContact.Add(t);
+            db.SaveChanges();
+            return PartialView();
+        }
+
+
+    }
+}
---
+++ MvcCv/Repositories/DeneyimRepository.cs
@@ -0,0 +1,20 @@
+﻿using MvcCv.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCv.Repositories
+{
+    public class DeneyimRepository
+    {
+        DbCvEntities db = new DbCvEntities();
+        public List<TblExperience> List()
+        {
+            return db.TblExperience.ToList();
+
+        }
+
+         
+    }
+}
